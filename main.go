2ee9640352b1f90b7b8341008dfc8cc9debf0c06
David Baldwwynn <david.baldwin@alumni.ubc.ca>
2016-02-28 17:33:58 -0800

    Got engcore login to work

--- main.go
+++ main.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-	"crypto/rsa"
+	//"crypto/rsa"
 	"errors"
-	"flag"
-	"io/ioutil"
-	"log"
+	//"flag"
+	//"io/ioutil"
+	//"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
 	"time"
-
+	"strconv"
+	"github.com/headzoo/surf"
+	"github.com/headzoo/surf/jar"
 	"github.com/PuerkitoBio/goquery"
-	"github.com/jinzhu/gorm"
-
-	_ "github.com/mattn/go-sqlite3"
+	//"github.com/jinzhu/gorm"
+	"github.com/headzoo/surf/browser"
+	//_ "github.com/mattn/go-sqlite3"
+	"fmt"
+	"bytes"
 )
 
 type User struct {
-	Username, Password, University string
-	LastActivated                  time.Time
-	Encrypted                      bool
+	Username, Password             string
 }
 
 func (u User) Validate() error {
-	if u.University != "ubc" {
-		return errors.New("Only UBC is currently supported.")
-	}
 	if len(u.Username) == 0 {
 		return errors.New("Invalid username.")
 	}
@@ -36,144 +35,193 @@ func (u User) Validate() error {
 	return nil
 }
 
-func (u *User) Activate() error {
-	jar, err := cookiejar.New(nil)
+var (
+	c = make(chan string)
+)
+
+func (u *User) loginToEngCore() (*browser.Browser, error) {
+	bow := surf.NewBrowser()
+	bow.SetCookieJar(jar.NewMemoryCookies())
+	err := bow.Open("https://www.ubcengcore.com/students")
 	if err != nil {
-		return err
+		return bow, err
 	}
-	c := &http.Client{
+	// Click the login button
+	err = bow.Click(".customContentContainer > p:nth-child(2) > a:nth-child(1)")
+	if err != nil {
+		return bow, err
+	}
+	
+	// Log into shibboleth	
+	// Should output "The University of British Columbia"
+	fmt.Println(bow.Title())
+	
+	form, form_err := bow.Form("form[name='loginForm']")
+	if form_err != nil {
+		return bow, form_err
+	}
+
+	form.Input("j_username", u.Username)
+	form.Input("j_password", u.Password)
+	
+	err = form.Submit()
+	if err != nil {
+		return bow, err
+	}
+
+	jar, err := cookiejar.New(nil)
+	jar.SetCookies(bow.Url(), bow.SiteCookies())
+
+	if err != nil {
+		return bow, err
+	}
+	client := &http.Client{
 		Jar: jar,
 	}
-	resp, err := c.PostForm("https://upassbc.translink.ca/", url.Values{"PsiId": {u.University}})
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	//body, _ := ioutil.ReadAll(resp.Body)
-	//log.Println(string(body))
-
-	// Log into shibboleth
-	resp2, err := c.PostForm("https://shibboleth2.id.ubc.ca/idp/Authn/UserPassword", url.Values{
-		"j_username": {u.Username},
-		"j_password": {u.Password},
-		"action":     {"Continue"},
-	})
-	if err != nil {
-		return err
-	}
-	defer resp2.Body.Close()
-
+	
 	// Continue SAML
-	doc, err := goquery.NewDocumentFromResponse(resp2)
+	r := bytes.NewReader([]byte( bow.Body() ))
+	doc, err := goquery.NewDocumentFromReader(r)
 	if err != nil {
-		return err
+		return bow, err
 	}
 	action := doc.Find("form").AttrOr("action", "")
 	relayState := doc.Find("input[name=\"RelayState\"]").AttrOr("value", "")
 	samlResponse := doc.Find("input[name=\"SAMLResponse\"]").AttrOr("value", "")
 
-	resp3, err := c.PostForm(action, url.Values{
+	resp3, err := client.PostForm(action, url.Values{
 		"RelayState":   {relayState},
 		"SAMLResponse": {samlResponse},
 		"action":       {"Continue"},
 	})
 	if err != nil {
-		return err
+		fmt.Println("Error with response3")
+		return bow, err
 	}
+	fmt.Println("got past SAML")
 	defer resp3.Body.Close()
-
-	// Continue upassbc login
-	doc2, err := goquery.NewDocumentFromResponse(resp3)
-	if err != nil {
-		return err
-	}
-	action = doc2.Find("form").AttrOr("action", "")
-	wa := doc2.Find("input[name=\"wa\"]").AttrOr("value", "")
-	wresult := doc2.Find("input[name=\"wresult\"]").AttrOr("value", "")
-
-	resp4, err := c.PostForm(action, url.Values{
-		"wa":      {wa},
-		"wresult": {wresult},
-		"action":  {"Submit"},
-	})
-	if err != nil {
-		return err
-	}
-	defer resp4.Body.Close()
-
-	doc3, err := goquery.NewDocumentFromResponse(resp4)
-	if err != nil {
-		return err
-	}
-	if doc3.Find("#form-request").Length() == 0 {
-		return errors.New("Invalid username or password details.")
-	}
-
-	action = "https://upassbc.translink.ca" + doc3.Find("#form-request").AttrOr("action", "")
-	vals := url.Values{"requestButton": {"Request"}}
-	checkboxes := doc3.Find("#form-request input:nth-child(1)[type=\"checkbox\"]")
-	log.Printf("Activating UPass %s, count %d", u.Username, checkboxes.Length())
-	if checkboxes.Length() == 0 {
-		return nil
-	}
-	doc3.Find("#form-request input").Each(func(i int, sel *goquery.Selection) {
-		name := sel.AttrOr("name", "")
-		val := sel.AttrOr("value", "")
-		if len(name) == 0 || len(val) == 0 {
-			return
-		}
-		vals[name] = append(vals[name], val)
-	})
-	boxName := checkboxes.Last().AttrOr("name", "")
-	vals[boxName] = append(vals[boxName], "true")
-
-	log.Println("vals", vals)
-	resp5, err := c.PostForm(action, vals)
-	if err != nil {
-		return err
-	}
-	defer resp5.Body.Close()
-	body, _ := ioutil.ReadAll(resp5.Body)
-	log.Println(string(body))
-
-	u.LastActivated = time.Now()
-
-	return nil
+	
+	return bow, nil
 }
 
-func activateEverything(db gorm.DB, key *rsa.PrivateKey) {
-	log.Println("Checking for new UPasses...")
-	var users []*User
-	db.Find(&users)
-	for i, user := range users {
-		if err := user.Decrypt(key); err != nil {
-			log.Printf("ERR decrypting %s, %s", user.Username, err)
-			continue
-		}
-		if err := user.Activate(); err != nil {
-			log.Printf("ERR activating %s, %s", user.Username, err)
-			continue
-		}
-		if err := user.Decrypt(key); err != nil {
-			log.Printf("ERR decrypting %s, %s", user.Username, err)
-			continue
-		}
-		db.Model(user).Update("last_activated", user.LastActivated)
-		// Remove decrypted version from memory.
-		users[i] = nil
+//Record engcore pingback times
+func pingEngCore(bow *browser.Browser) {
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		panic(err)
 	}
-}
+	jar.SetCookies(bow.Url(), bow.SiteCookies())
 
-func pollActivator(db gorm.DB, key *rsa.PrivateKey) {
-	ticker := time.NewTicker(24 * time.Hour)
-	for _ = range ticker.C {
-		activateEverything(db, key)
+
+	//Test that we can ping site without redirects
+	client := &http.Client{
+		Jar: jar,
 	}
+
+	//Url that we should use to test pingback 
+	pingUrl := bow.Url().String()
+	if err != nil {
+		panic(err)	
+	}
+	
+	timeoutStart := time.Now()
+	var totalPing int64 = 0
+	var numPing int64 = 0
+	var avgPing int64 = 0
+
+	//Loop while our seesion hasn't expired
+	for err == nil {
+		//lag timer start
+		start := time.Now()
+
+		res, err := client.Get(pingUrl)
+		if err != nil {
+			msg := "Error:" + err.Error()
+			c <- msg
+		} else {
+			lag := time.Since(start)
+			var msg string
+			
+			numPing++
+			totalPing = totalPing + int64(lag) 
+
+			//	running slow
+			if lag > time.Duration(300)*time.Second {
+				msg = pingUrl + " lag: " + lag.String()
+			}
+
+			msg = pingUrl + ", lag: " + lag.String()
+			c <- msg
+
+			res.Body.Close()
+		}
+	}
+	
+	timeoutPeriod := time.Since(timeoutStart)
+	avgPing = totalPing/numPing
+	
+	//This is a bit hacky
+	msg := "Timeout Period was:" + timeoutPeriod.String() + " average ping lag: " + strconv.FormatInt(avgPing, 10) 
+	c <- msg
+	close(c) 
 }
+/*
+	func activateEverything(db gorm.DB, key *rsa.PrivateKey) {
+		log.Println("Checking for new UPasses...")
+		var users []*User
+		db.Find(&users)
+		for i, user := range users {
+			if err := user.Decrypt(key); err != nil {
+				log.Printf("ERR decrypting %s, %s", user.Username, err)
+				continue
+			}
+			if err := user.Activate(); err != nil {
+				log.Printf("ERR activating %s, %s", user.Username, err)
+				continue
+			}
+			if err := user.Decrypt(key); err != nil {
+				log.Printf("ERR decrypting %s, %s", user.Username, err)
+				continue
+			}
+			db.Model(user).Update("last_activated", user.LastActivated)
+			// Remove decrypted version from memory.
+			users[i] = nil
+		}
+	}
 
-var addr = flag.String("addr", ":3000", "The address to listen on.")
+	func pollActivator(db gorm.DB, key *rsa.PrivateKey) {
+		ticker := time.NewTicker(24 * time.Hour)
+		for _ = range ticker.C {
+			activateEverything(db, key)
+		}
+	}
 
+	var addr = flag.String("addr", ":3000", "The address to listen on.")
+*/
 func main() {
+	user := &User{
+		Username: "davidb7",
+		Password: "ptW7$7MM",
+	}
+	
+	_, err := user.loginToEngCore()
+	fmt.Println("Finished logging into EngCore")
+	if err != nil {
+		fmt.Println("Error is: " +err.Error())
+		panic(err)
+	}
+
+	//pingEngCore(bow)
+	// output logs to the terminal
+	//for i := range c {
+	//	fmt.Println(i)
+	//}
+
+	fmt.Println("Done")
+	return
+
+	/*
 	flag.Parse()
 
 	db, err := gorm.Open("sqlite3", "./user.db")
@@ -188,17 +236,17 @@ func main() {
 		log.Fatal(err)
 	}
 
+	
 	go pollActivator(db, key)
 	go activateEverything(db, key)
 
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.HandleFunc("/api/v1/register", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/api/v1/test", func(w http.ResponseWriter, r *http.Request) {
 		if err := r.ParseForm(); err != nil {
 			http.Error(w, err.Error(), 400)
 			return
 		}
 		user := &User{
-			University: r.FormValue("university"),
 			Username:   r.FormValue("username"),
 			Password:   r.FormValue("password"),
 		}
@@ -222,4 +270,5 @@ func main() {
 	})
 	log.Printf("Listening on %s", *addr)
 	log.Fatal(http.ListenAndServe(*addr, nil))
+	*/
 }
